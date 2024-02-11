a094c004865e990c771c0bcf1a2d82e72848d8f3
Jaco van den Bosch <jacovdboschjr@gmail.com>
2024-02-11 19:20:29 +0100

    initial

---
+++ library_user/bin/library_user.dart
@@ -0,0 +1,5 @@
+import 'package:library_user/library_user.dart' as library_user;
+
+void main(List<String> arguments) {
+  print('Hello world: ${library_user.calculate()}!');
+}
---
+++ library_user/test/library_user_test.dart
@@ -0,0 +1,8 @@
+import 'package:library_user/library_user.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
