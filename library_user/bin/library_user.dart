1223464830a99f48e9eb998fe10500d274bbf93c
Jaco van den Bosch <jacovdboschjr@gmail.com>
2024-02-11 19:40:16 +0100

    call rust from dart!

--- library_user/bin/library_user.dart
+++ library_user/bin/library_user.dart
@@ -1,5 +1,26 @@
-import 'package:library_user/library_user.dart' as library_user;
+import 'dart:io' show Platform, Directory;
+import 'dart:ffi' as ffi;
+import 'package:path/path.dart' as path;
+
+typedef print_person_func = ffi.Void Function();
+typedef PrintPerson = void Function();
 
 void main(List<String> arguments) {
-  print('Hello world: ${library_user.calculate()}!');
+  var libraryPath = path.join(Directory.current.path,
+      '../shared_library/target/release', 'libshared_library.so');
+
+  if (Platform.isMacOS) {
+    libraryPath = path.join(Directory.current.path,
+        '../shared_library/target/release', 'libshared_library.dylib');
+  } else if (Platform.isWindows) {
+    libraryPath = path.join(Directory.current.path,
+        '../shared_library/target/release', 'shared_library.dll');
+  }
+
+  var dylib = ffi.DynamicLibrary.open(libraryPath);
+
+  final print_person =
+      dylib.lookupFunction<print_person_func, PrintPerson>('print_person');
+
+  print_person();
 }
